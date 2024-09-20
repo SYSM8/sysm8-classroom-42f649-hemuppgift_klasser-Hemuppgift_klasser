bb32ed5564287bfe7f55be7c8b6a71d7331c9fba
NanEkman <nen_92@hotmail.com>
2024-09-20 17:19:07 +0200

    Ändrat till engelska på utskrift samt färdigställt koden

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -24,29 +24,42 @@ namespace BankAccount
 
         //Lägg till Metoder
 
-        public void Deposit(double balance)
+        public void Deposit(double amount)
         {
             // tar ett decimal belopp som parameter och lägger till det till saldot.
+            Console.WriteLine("How much money would you like to deposit?");
+            amount = double.Parse(Console.ReadLine());
 
+            Balance += amount;
+            Console.WriteLine($"Deposited: {amount}, New balance: {Balance}");
+            
         }
 
-        public void Withdraw(double balance) 
+        public void Withdraw(double amount) 
         {
-            // tar ett decimal belopp som parameter och drar av det från saldot om det finns tillräckligt med pengar på
-            //kontot.Annars ska metoden meddela att uttaget inte kan genomföras.
-            if (Balance < balance)
-            {
-                Balance = balance;
-            }
+            // tar ett decimal belopp som parameter och drar av det från saldot om det finns tillräckligt med pengar på kontot. 
+            //Annars ska metoden meddela att uttaget inte kan genomföras.
 
+            Console.WriteLine("How much money would you like to withdraw?");
+            amount = double.Parse(Console.ReadLine());
+
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdew: {amount}, New balance: {Balance}");
+            }
+            else
+            { 
+                Console.WriteLine($"You don't have enough money to withdraw {amount}.");
+            }
         }
 
         public void DisplayBalance()
         {
             // visar det aktuella saldot på kontot
-            Console.WriteLine(Balance);
+            Console.WriteLine($"Current balance: {Balance}");
         }
 
-        //Lycka till! :)
+        //Lycka till! :) 
     }
 }
