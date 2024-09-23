b88913b47ee713bc1cfa027d5174bbe32b7c2c26
Ture Christenson <ture.christenson@edu.newton.se>
2024-09-23 10:07:18 +0200

    Add files via upload

---
+++ sysm8-classroom-42f649-hemuppgift_klasser-Hemuppgift_klasser/BankAccount/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Jane", "Smith", 30000);
+            
+            
+
+
+            Console.WriteLine($"Annual Salary: {employee.GetAnnualSalary()}");
+
+            employee.GiveRaise(3);
+
+            Console.WriteLine($"New Monthly Salary: {employee.Salary/12}");
+            Console.WriteLine($"New Annual Salary: {employee.GetAnnualSalary()}");
+
+            BankAccount account = new BankAccount("123456789", "John Doe", 1000);
+            account.Deposit(500);
+            account.Withdraw(200);
+            account.DisplayBalance();
+
+
+
+
+
+
+        }
+    }
+}
