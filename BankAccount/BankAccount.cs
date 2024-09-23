42724c0837aaa8f8aae72263ac03a5d859fb0a4b
NanEkman <nen_92@hotmail.com>
2024-09-23 09:56:25 +0200

    slutgiltig kod

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -14,8 +14,8 @@ namespace BankAccount
         public double Balance { get; set; }
 
         //Lägg till Konstruktor
-        public BankAccount(string accountNumber, string accountHolder, double balance) 
-        { 
+        public BankAccount(string accountNumber, string accountHolder, double balance)
+        {
             AccountNumber = accountNumber;
             AccountHolder = accountHolder;
             Balance = balance;
@@ -23,26 +23,41 @@ namespace BankAccount
 
 
         //Lägg till Metoder
-
-        public void Deposit(double balance)
+        public void Deposit(double amount)
         {
             // tar ett decimal belopp som parameter och lägger till det till saldot.
+            Console.WriteLine("How much money would you like to deposit?");
+            amount = double.Parse(Console.ReadLine());
 
+            Balance += amount;
+            Console.WriteLine($"Deposited: {amount}, New balance: {Balance}");
         }
 
-        public void Withdraw(double balance) 
+        public void Withdraw(double amount)
         {
-            // tar ett decimal belopp som parameter och drar av det från saldot om det finns tillräckligt med pengar på
-            //kontot.Annars ska metoden meddela att uttaget inte kan genomföras.
+            // tar ett decimal belopp som parameter och drar av det från saldot om det finns tillräckligt med pengar på kontot. 
+            //Annars ska metoden meddela att uttaget inte kan genomföras.
 
+            Console.WriteLine("How much money would you like to withdraw?");
+            amount = double.Parse(Console.ReadLine());
 
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdew: {amount}, New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough money to withdraw {amount}.");
+            }
         }
 
         public void DisplayBalance()
         {
             // visar det aktuella saldot på kontot
+            Console.WriteLine($"Current balance: {Balance}");
         }
 
-        //Lycka till! :)
+        //Lycka till! :) 
     }
 }
